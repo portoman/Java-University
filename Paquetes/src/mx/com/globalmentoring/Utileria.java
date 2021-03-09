395e0150f2e06d3a177d1fafc20475f2b731b2e7
portoman <portbuj@yahoo.es>
2021-03-09 17:51:58 +0100

    Create Utileria.java

---
+++ Paquetes/src/mx/com/globalmentoring/Utileria.java
@@ -0,0 +1,13 @@
+
+package mx.com.globalmentoring;
+
+
+public class Utileria {
+  
+    public static void imprimir (String s) {
+        
+        System.out.println("s = " + s);
+        
+    }
+    
+}
