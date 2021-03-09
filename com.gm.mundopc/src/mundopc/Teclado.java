fea570c452780fd8e761aed7c3ac1ce2540eb368
portoman <portbuj@yahoo.es>
2021-03-09 17:57:09 +0100

    Create Teclado.java

---
+++ com.gm.mundopc/src/mundopc/Teclado.java
@@ -0,0 +1,21 @@
+package mundopc;
+
+public class Teclado extends DispositivoEntrada{
+
+    private int idTeclado;
+    private static int contadorTeclados;
+
+    public Teclado(String tipoEntrada, String marca) {
+        super(tipoEntrada, marca);
+        this.idTeclado = ++Teclado.contadorTeclados;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Teclado{" + "idTeclado=" + idTeclado +" , "+super.getTipoEntrada()+ '}';
+    }
+
+
+}
+
